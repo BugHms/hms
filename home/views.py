7eb2e7ee00f724b3f3308b7b52c348a0b5d34186
thisisazeez <imfostersyndrome@gmail.com>
2022-08-05 19:12:40 +0100

    removed: removed request greetings api because of pythonanywhere deployment issues

--- home/views.py
+++ home/views.py
@@ -3,14 +3,14 @@ from django.views.generic import TemplateView
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-import requests
+# import requests
 
 
 # Create your views here.
 @login_required()
 def home(request):
-    response = requests.get("https://www.greetingsapi.com/random")
-    json_data = response.json()
-    # print(json_data)
+    # response = requests.get("https://www.greetingsapi.com/random")
+    # json_data = response.json()
+    # # print(json_data)
     messages.add_message(request, messages.INFO, 'Welcome to The Hospital Portal.')
     return render(request, 'home/base.html', {'greetings': json_data})
