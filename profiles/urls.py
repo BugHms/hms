63aede22eb039ba3592ebe2b7960cec028906b04
thisisazeez <imfostersyndrome@gmail.com>
2022-08-05 19:16:12 +0100

    removed modules that cause us to use url and commented url out for pythonanywhere

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
+# from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
